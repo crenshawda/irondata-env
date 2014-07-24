2e46f1c339449bc7aeb64d24aa7c3be25a1b3f4a
Dennis Crenshaw <dennis.crenshaw@irondata.com>
2014-07-24 13:11:00 -0400

    automate the data loading for icmdb

--- modules/icmdb-schema.sh
+++ modules/icmdb-schema.sh
@@ -13,4 +13,28 @@ cd Code
 #sed -i 's/liquibase --defaultsFile=\$1.properties --changeLogFile=install.xml --logLevel=severe update/liquibase --defaultsFile=\$1.properties --changeLogFile=install.xml --logLevel=debug update/' /home/vagrant/Code/liquibase/install.sh
 #chmod +x /home/vagrant/Code/liquibase/install.sh
 #cd liquibase
+#./dropAll.sh mysql
 #./install.sh mysql
+#./update.sh mysql
+
+# Automate the workbook data-upload
+#cd /home/vagrant/Code/
+# Schema
+#svn co https://svn.irondatacorp.com/svn/icm-20/clientApps/wiiris/database/trunk/ icmdb-schema
+# Data
+#svn co https://svn.irondatacorp.com/svn/everest/artifacts/wiiris/trunk/technical/database/dataLoad/ icmdb-data
+# Utility 
+#svn co https://svn.irondatacorp.com/svn/icm-20/clientApps/wiiris/wiiris-dataload/trunk icmdb-dataloader
+#cd icmdb-dataloader
+#grails run-app
+
+#Load Order-
+#1- baseRefDataICM.xlsx
+#2- baseRefDataIRIS.xlsx
+#3- qaBaseRefDataIRIS.xlsx
+#4- Postcode1.xlsx  **
+
+#curl -XPOST -F file=@/vagrant/Code/icmdb-data/xxx.xlsx http://localhost:8080/wiiris-dataload/dataload/upload
+
+# stop the app-server?
+#grails stop-app?
